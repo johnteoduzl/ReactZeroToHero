d213411d3552e8edc39d6fa80fa479caaa133217
John Odindo <johnteoduzl@users.noreply.github.com>
2025-01-22 09:14:18 -0800

    First Component with JSX Function
    Ammended the App.js file and added the Product Card function to return html as JSX and attached it to the main function

--- react-crash-course/src/App.js
+++ react-crash-course/src/App.js
@@ -1,10 +1,33 @@
 //import logo from './logo.svg';
 import './App.css';
 
+function ProductCard()
+{
+  return(
+    <article>
+      <h2>iPhone 15 pro</h2>
+      <img src="images/iphone.png"
+            alt="iPhone 15 Pro"
+            width="128px"
+            height="128px"
+            />
+
+            <p>Specifications</p>
+            <ul>
+              <li>A17 Pro chip with 6-core GPU</li>
+              <li>3x or 5x telephoto camera</li>
+              <li>Up to 29 hours video playback</li>
+            </ul>
+            <button>Buy (From $999)</button>
+    </article>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       React Crash Course
+      <ProductCard/>
     </div>
   );
 }
