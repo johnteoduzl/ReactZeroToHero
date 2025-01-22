0528cfb32f684599b565498c459dfd451fd3ef79
John Odindo <johnteoduzl@users.noreply.github.com>
2025-01-22 09:50:58 -0800

    Functions and Styles in JS
    Added a function to get title and passed all parameters as objects of a product. Additionally also added the styles tag that uses double curly braces.

--- react-crash-course/src/components/ProductCard.js
+++ react-crash-course/src/components/ProductCard.js
@@ -1,22 +1,39 @@
 
 export function ProductCard()
 {
+
+    const product = {
+        imageSrc:"images/apple.png",
+        title:"iPhone 15 Pro",
+        specification:[
+            "A17 Pro chip with 6-core GPU",
+            "3x or 5x telephoto camera",
+            "Up to 29 hours video playback"
+        ],
+        price:999,
+    };
+
+    function getProductTitle()
+    {
+        return product.title;
+    }
+
   return(
-    <article>
-      <h2>iPhone 15 pro</h2>
-      <img src="images/apple.png"
-            alt="iPhone 15 Pro"
+    <article style={{ border: '1px solid white', borderRadius: '8px', padding: '16px',textAlign:'center'}}>
+      <h2>{getProductTitle()}</h2>
+      <img src={product.imageSrc}
+            alt={product.title}
             width="128px"
             height="128px"
             />
 
             <p>Specifications</p>
             <ul>
-              <li>A17 Pro chip with 6-core GPU</li>
-              <li>3x or 5x telephoto camera</li>
-              <li>Up to 29 hours video playback</li>
+              <li>{product.specification[0]}</li>
+              <li>{product.specification[1]}</li>
+              <li>{product.specification[2]}</li>
             </ul>
-            <button>Buy (From $999)</button>
+            <button>Buy (From ${product.price})</button>
     </article>
   );
 }
