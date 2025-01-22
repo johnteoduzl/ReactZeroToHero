9b9b0c38efa59e6c1c4591acc513757b01a3b88b
John Odindo <johnteoduzl@users.noreply.github.com>
2025-01-22 09:23:17 -0800

    Export and import of JSX files
    I have added the components folder and added the export and import commands to both the Product Card and the App.js file

--- react-crash-course/src/App.js
+++ react-crash-course/src/App.js
@@ -1,27 +1,6 @@
 //import logo from './logo.svg';
 import './App.css';
-
-function ProductCard()
-{
-  return(
-    <article>
-      <h2>iPhone 15 pro</h2>
-      <img src="images/apple.png"
-            alt="iPhone 15 Pro"
-            width="128px"
-            height="128px"
-            />
-
-            <p>Specifications</p>
-            <ul>
-              <li>A17 Pro chip with 6-core GPU</li>
-              <li>3x or 5x telephoto camera</li>
-              <li>Up to 29 hours video playback</li>
-            </ul>
-            <button>Buy (From $999)</button>
-    </article>
-  );
-}
+import { ProductCard } from './components/ProductCard';
 
 function App() {
   return (
---
+++ react-crash-course/src/components/ProductCard.js
@@ -0,0 +1,23 @@
+
+export function ProductCard()
+{
+  return(
+    <article>
+      <h2>iPhone 15 pro</h2>
+      <img src="images/apple.png"
+            alt="iPhone 15 Pro"
+            width="128px"
+            height="128px"
+            />
+
+            <p>Specifications</p>
+            <ul>
+              <li>A17 Pro chip with 6-core GPU</li>
+              <li>3x or 5x telephoto camera</li>
+              <li>Up to 29 hours video playback</li>
+            </ul>
+            <button>Buy (From $999)</button>
+    </article>
+  );
+}
+
