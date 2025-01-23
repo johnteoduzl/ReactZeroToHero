8bf317aa61467fb700bd931788b747fdd8078e6d
John Odindo <johnteoduzl@users.noreply.github.com>
2025-01-23 04:46:06 -0800

    Props addition
    Addded the fucntionality of sharing information between JSX using props.

--- react-crash-course/src/App.js
+++ react-crash-course/src/App.js
@@ -3,10 +3,20 @@ import './App.css';
 import { ProductCard } from './components/ProductCard';
 
 function App() {
+  const product = {
+    imageSrc:"images/apple.png",
+    title:"iPhone 15 Pro",
+    specification:[
+        "A17 Pro chip with 6-core GPU",
+        "3x or 5x telephoto camera",
+        "Up to 29 hours video playback"
+    ],
+    price:999,
+};
   return (
     <div className="App">
       React Crash Course
-      <ProductCard/>
+      <ProductCard product={product}/>
     </div>
   );
 }
--- react-crash-course/src/components/ProductCard.js
+++ react-crash-course/src/components/ProductCard.js
@@ -1,39 +1,27 @@
 
-export function ProductCard()
+export function ProductCard(props)
 {
-
-    const product = {
-        imageSrc:"images/apple.png",
-        title:"iPhone 15 Pro",
-        specification:[
-            "A17 Pro chip with 6-core GPU",
-            "3x or 5x telephoto camera",
-            "Up to 29 hours video playback"
-        ],
-        price:999,
-    };
-
     function getProductTitle()
     {
-        return product.title;
+        return props.product.title;
     }
 
   return(
     <article style={{ border: '1px solid white', borderRadius: '8px', padding: '16px',textAlign:'center'}}>
-      <h2>{getProductTitle()}</h2>
-      <img src={product.imageSrc}
-            alt={product.title}
+      <h2>{props.product.title}</h2>    
+      <img src={props.product.imageSrc}
+            alt={props.product.title}
             width="128px"
             height="128px"
             />
 
             <p>Specifications</p>
             <ul>
-              <li>{product.specification[0]}</li>
-              <li>{product.specification[1]}</li>
-              <li>{product.specification[2]}</li>
+              <li>{props.product.specification[0]}</li>
+              <li>{props.product.specification[1]}</li>
+              <li>{props.product.specification[2]}</li>
             </ul>
-            <button>Buy (From ${product.price})</button>
+            <button>Buy (From ${props.product.price})</button>
     </article>
   );
 }
