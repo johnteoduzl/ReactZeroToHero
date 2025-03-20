ce25e088cec22e86373d34ce74cd33b1ad0d2bba
John Odindo <johnteoduzl@users.noreply.github.com>
2025-03-20 08:55:54 -0700

    Button Click Handler to Product Card
    Added the products to pass different infirmation in the App.js file and also added the handleClick function and ammended the onClick function to prevent calling of function on loading of information

--- react-crash-course/src/App.js
+++ react-crash-course/src/App.js
@@ -4,23 +4,41 @@ import { ProductCard } from './components/ProductCard';
 import { ProductList } from './components/ProductList';
 
 function App() {
-  const product = {
+  const product = [{
     imageSrc:"images/apple.png",
-    title:"iPhone 15 Pro",
+    title:"iPhone 13 Pro",
     specification:[
-        "A17 Pro chip with 6-core GPU",
+        "A13 Pro chip with 6-core GPU",
         "3x or 5x telephoto camera",
         "Up to 29 hours video playback"
     ],
     price:999,
-};
+},{
+  imageSrc:"images/apple.png",
+  title:"iPhone 16 Pro",
+  specification:[
+      "A17 Pro chip with 6-core GPU",
+      "3x or 5x telephoto camera",
+      "Up to 29 hours video playback"
+  ],
+  price:1999,
+},{
+  imageSrc:"images/apple.png",
+  title:"iPhone 17 Pro",
+  specification:[
+      "A19 Pro chip with 6-core GPU",
+      "3x or 5x telephoto camera",
+      "Up to 29 hours video playback"
+  ],
+  price:3999,
+}];
   return (
     <div className="App">
       React Crash Course
       <ProductList>
-      <ProductCard width='96px' height = '96px' background='peru' product={product}/>
-      <ProductCard width='64px' height = '64px' product={product}/>
-      <ProductCard width='128px' height = '128px' background='darkolivegree' product={product}/>
+      <ProductCard width='96px' height = '96px' background='peru' product={product[0]}/>
+      <ProductCard width='64px' height = '64px' product={product[1]}/>
+      <ProductCard width='128px' height = '128px' background='darkolivegree' product={product[2]}/>
       </ProductList>
       
     </div>
--- react-crash-course/src/components/ProductCard.js
+++ react-crash-course/src/components/ProductCard.js
@@ -5,6 +5,10 @@ export function ProductCard(props, background = 'slategrey', ...restProps) {
     return produ.title;//props.product.title;
   }
 
+function handleClick(product)
+{
+alert(`You clicked on ${getProductTitle()} which cost ${props.product.price}`);
+}
 
   return (
     <article style={{ width: '100%', border: '3px solid white', borderRadius: '9px', padding: '16px', textAlign: 'center' }}>
@@ -22,7 +26,7 @@ export function ProductCard(props, background = 'slategrey', ...restProps) {
         <li>{props.product.specification[1]}</li>
         <li>{props.product.specification[2]}</li>
       </ul>
-      <button>Buy (From ${props.product.price})</button>
+      <button onClick={()=>handleClick(props.product)}>Buy (From ${props.product.price})</button>
     </article>
   );
 }
